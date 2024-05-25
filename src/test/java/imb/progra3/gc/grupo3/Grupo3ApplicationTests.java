b770ab4a416d090c694a26eedf5bde5c4934d453
Mario Ahumada <ahumadamob@gmail.com>
2024-05-25 09:58:12 -0300

    Generado vacío con Spring Initlzr

---
+++ src/test/java/imb/progra3/gc/grupo3/Grupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package imb.progra3.gc.grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
