85fc78f44695742ac4345bb0b3daf1097df7592d
florenciazoni <florzoni@gmail.com>
2024-05-29 13:40:11 -0300

    creacion entidad transaccion

---
+++ src/main/java/imb/progra3/gc/grupo3/entity/Transaccion.java
@@ -0,0 +1,53 @@
+package imb.progra3.gc.grupo3.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transaccion {
+
+    private Long id;
+    private String tipoTransaccion;
+    private BigDecimal monto;
+    private LocalDateTime fechaHora;
+    private Long idCuenta;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipoTransaccion() {
+        return tipoTransaccion;
+    }
+
+    public void setTipoTransaccion(String tipoTransaccion) {
+        this.tipoTransaccion = tipoTransaccion;
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public Long getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(Long idCuenta) {
+        this.idCuenta = idCuenta;
+    }
+}
