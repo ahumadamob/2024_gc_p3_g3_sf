8e40ed304984bd724ad08b2349061f085a58689b
BeelSoria <ma.belen.soriaa@gmail.com>
2024-06-13 02:16:22 -0300

    subida de archivos para la persistencia de la entidad Tarjeta

---
+++ src/main/java/com/cooweb/persistenciatarjetaApplication.java
@@ -0,0 +1,13 @@
+package com.cooweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class persistenciatarjetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(persistenciatarjetaApplication.class, args);
+	}
+
+}
