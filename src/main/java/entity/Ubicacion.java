b8139377849248aefe4c01f8574db0777f6230f4
Martina <animartics@gmail.com>
2024-06-30 19:32:40 -0300

    Añadir entidad Ubicación

--- src/main/java/entity/Ubicacion.java
+++ src/main/java/entity/Ubicacion.java
@@ -1,47 +1,24 @@
 package entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-@Entity
 public class Ubicacion {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String pais;
-    private String ciudad;
-    private String direccion;
-
-
-    public Ubicacion() {
-    }
-
-
-    public Ubicacion(String pais, String ciudad, String direccion) {
-        this.pais = pais;
-        this.ciudad = ciudad;
-        this.direccion = direccion;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getCiudad() {
+	private Long id;
+	private String pais;
+	private String ciudad;
+	private String direccion;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public String getCiudad() {
         return ciudad;
     }
 
