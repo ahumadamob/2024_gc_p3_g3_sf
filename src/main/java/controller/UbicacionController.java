0a4c2a61f0c5d5ddf09c245252dd432a90cb7d83
Martina <animartics@gmail.com>
2024-06-30 20:50:40 -0300

    Agrego carpeta controller con clase UbicacionController

---
+++ src/main/java/controller/UbicacionController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import entity.Ubicacion;
+import service.IUbicacionService;
+
+@RestController
+@RequestMapping("/api/ubicaciones")
+public class UbicacionController {
+	private final IUbicacionService ubicacionService;
+	
+	public UbicacionController(IUbicacionService ubicacionService) {
+		this.ubicacionService = ubicacionService;
+	}
+	@GetMapping
+    public List<Ubicacion> getAll() {
+        return ubicacionService.getAll();
+    }
+	@GetMapping("/{id}")
+    public ResponseEntity<Ubicacion> getById(@PathVariable Long id) {
+        Ubicacion ubicacion = ubicacionService.getById(id);
+        if (ubicacion != null) {
+            return ResponseEntity.ok(ubicacion);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+	 @PostMapping
+	    public Ubicacion create(@RequestBody Ubicacion ubicacion) {
+	        return ubicacionService.save(ubicacion);
+	    }
+	 @PutMapping("/{id}")
+	    public ResponseEntity<Ubicacion> update(@PathVariable Long id, @RequestBody Ubicacion ubicacion) {
+	        if (ubicacionService.exists(id)) {
+	            ubicacion.setId(id);
+	            Ubicacion updatedUbicacion = ubicacionService.save(ubicacion);
+	            return ResponseEntity.ok(updatedUbicacion);
+	        } else {
+	            return ResponseEntity.notFound().build();
+	        }
+	    }
+	 @DeleteMapping("/{id}")
+	    public ResponseEntity<Void> delete(@PathVariable Long id) {
+	        if (!ubicacionService.exists(id)) {
+	            return ResponseEntity.notFound().build();
+	        }
+	        ubicacionService.delete(id);
+	        return ResponseEntity.noContent().build();
+	    }
+}
