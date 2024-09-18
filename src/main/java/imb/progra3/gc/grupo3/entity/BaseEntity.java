33d01d0481205e8205219be8db9c4c95fc93e9f2
AniitaMacoMendoza <nanito9115@gmail.com>
2024-09-18 00:25:25 -0300

    Implementar BaseEntity

---
+++ src/main/java/imb/progra3/gc/grupo3/entity/BaseEntity.java
@@ -0,0 +1,46 @@
+package imb.progra3.gc.grupo3.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public abstract class BaseEntity {
+	
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    @Column(name = "created_at", updatable = false)
+	    private LocalDateTime createdAt;
+
+	    @Column(name = "updated_at")
+	    private LocalDateTime updatedAt;
+
+	    // Getters and Setters
+	    public Long getId() {
+	        return id;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public LocalDateTime getCreatedAt() {
+	        return createdAt;
+	    }
+
+	    public void setCreatedAt(LocalDateTime createdAt) {
+	        this.createdAt = createdAt;
+	    }
+
+	    public LocalDateTime getUpdatedAt() {
+	        return updatedAt;
+	    }
+
+	    public void setUpdatedAt(LocalDateTime updatedAt) {
+	        this.updatedAt = updatedAt;
+	    }
+	}
