c5b366b5377ed9666f629f92a2c96d0258be4dc6
maipzz <mmaipzz15@gmail.com>
2024-09-20 02:24:07 -0300

    persistencia
    persistencia completa de la entidad cliente

---
+++ src/main/java/imb/progra3/gc/grupo3/service/IClienteService.java
@@ -0,0 +1,13 @@
+package imb.progra3.gc.grupo3.service;
+
+import imb.progra3.gc.grupo3.entity.Cliente;
+import java.util.List;
+
+public interface IClienteService {
+    List<Cliente> getAll();
+    Cliente getById(Long id);
+    Cliente save(Cliente cliente);
+    void delete(Long id);
+    boolean exists(Long id);
+    List<Cliente> findClientesByEdad(int edad);
+}
